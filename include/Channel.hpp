e18252a6d7cb836d4dc1eb9fdbe0d12d6d2aa00c
Mohammed Abrar Shariar <mshariar@k2r4p2.42mulhouse.fr>
2026-08-16 02:14:41 +0200

    added channel after testing commands is working

--- include/Channel.hpp
+++ include/Channel.hpp
@@ -3,10 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   Channel.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hchowdhu <hchowdhu@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mshariar <mshariar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/13 00:11:01 by hchowdhu          #+#    #+#             */
-/*   Updated: 2026/07/13 00:11:04 by hchowdhu         ###   ########.fr       */
+/*   Updated: 2026/08/16 02:10:33 by mshariar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
+#include <string>
+#include <vector>
+
+class client;
+
+class Channel
+{
+    private:
+        std::string     _name;
+        std::vector<Client *>   _clients;
+        std::vector<Client *>   _operators;
+        Channel();
+        
+    public:
+        Channel(const std::string &name);
+		Channel(const Channel &other);
+		Channel &operator=(const Channel &other);
+		~Channel();
+
+        const   std::string &getName() const;
+        size_t  getClientCount() const;
+        bool    hasClient(Client  *client) const;
+        bool    isOperator(Client  *client) const;
+        void    addClient(Client  *client) const;
+        void    addOperator(Client  *client) const;
+        void broadcast(Client *sender, const std::string &msg);
+};
--- src/channel/Channel.cpp
+++ src/channel/Channel.cpp
@@ -3,10 +3,91 @@
 /*                                                        :::      ::::::::   */
 /*   Channel.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hchowdhu <hchowdhu@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mshariar <mshariar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/13 00:11:27 by hchowdhu          #+#    #+#             */
-/*   Updated: 2026/07/13 00:11:29 by hchowdhu         ###   ########.fr       */
+/*   Updated: 2026/08/16 02:13:37 by mshariar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Channel.hpp"
+#include "Client.hpp"
+#include "Server.hpp"
+
+Channel::Channel() {}
+
+Channel::Channel(const std::string &name) : _name(name), _clients(), _operators()
+{
+}
+
+Channel::Channel(const Channel &other)
+	: _name(other._name), _clients(other._clients),
+	  _operators(other._operators)
+{
+}
+
+Channel &Channel::operator=(const Channel &other)
+{
+	if (this != &other)
+	{
+		_name = other._name;
+		_clients = other._clients;
+		_operators = other._operators;
+	}
+	return (*this);
+}
+
+Channel::~Channel() {}
+
+const std::string &Channel::getName() const
+{
+	return (_name);
+}
+
+size_t Channel::getClientCount() const
+{
+	return (_clients.size());
+}
+
+bool Channel::hasClient(Client *client) const
+{
+	for (size_t i = 0; i < _clients.size(); ++i)
+	{
+		if (_clients[i] == client)
+			return (true);
+	}
+	return (false);
+}
+
+bool Channel::isOperator(Client *client) const
+{
+	for (size_t i = 0; i < _operators.size(); ++i)
+	{
+		if (_operators[i] == client)
+			return (true);
+	}
+	return (false);
+}
+
+void Channel::addClient(Client *client)
+{
+	if (client == NULL || hasClient(client))
+		return ;
+	_clients.push_back(client);
+}
+
+void Channel::addOperator(Client *client)
+{
+	if (client == NULL || isOperator(client))
+		return ;
+	_operators.push_back(client);
+}
+
+void Channel::broadcast(Client *sender, const std::string &msg)
+{
+	for (size_t i = 0; i < _clients.size(); ++i)
+	{
+		if (_clients[i] != sender)
+			_clients[i]->outputBuffer() += message + "\r\n";
+	}
+}
